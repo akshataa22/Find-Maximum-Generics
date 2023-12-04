a3ace755956c1d969cd906e934c742d542736858
akshataa22 <dhanawadeakshata53@gmail.com>
2023-12-05 00:40:27 +0530

    finding the maximum between three integers

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,7 +1,25 @@
 package Day13;
 
-public class MaxFinder {
+public class MaxFinder<T extends Comparable<T>> {
+    private T a;
+    private T b;
+    private T c;
+
+    public <Number extends Comparable> Number maxFind(Number x, Number y, Number z) {
+        Number max1 = x;
+        if (y.compareTo(max1) > 0) {
+            max1 = y;
+        } if(z.compareTo(max1) > 0) {
+            max1 = z;
+        }
+        return max1;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to maximum finding program using generics.");
+
+        MaxFinder<Integer> maximum = new MaxFinder<>();
+        System.out.println(maximum.maxFind(66, 77, 88));
     }
 }
